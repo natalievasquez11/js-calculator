d885c16ee80e199b1faf49153510396782b915bb
Natalie Vasquez <natalievasquez11@gmail.com>
2022-01-06 15:36:27 -0600

    more styling to page and calculator; made all grid-items inline-block buttons

--- src/components/calculatorContainer/CalculatorContainer.js
+++ src/components/calculatorContainer/CalculatorContainer.js
@@ -3,24 +3,24 @@ import './calculatorContainer.css';
 function CalculatorContainer() {
   return (
     <div className='calc-container'>
-      <div className="display grid-item">display</div>
+      <div className="display grid-item"><a href="#">display</a></div>
       <div className="AC grid-item"><a href="#">AC</a></div>
-      <div className="divide grid-item">/</div>
-      <div className="multiply grid-item">x</div>
-      <div className="seven grid-item">7</div>
-      <div className="eight grid-item">8</div>
-      <div className="nine grid-item">9</div>
-      <div className="subtract grid-item">-</div>
-      <div className="four grid-item">4</div>
-      <div className="five grid-item">5</div>
-      <div className="six grid-item">6</div>
-      <div className="add grid-item">+</div>
-      <div className="one grid-item">1</div>
-      <div className="two grid-item">2</div>
-      <div className="three grid-item">3</div>
-      <div className="equal grid-item">=</div>
-      <div className="zero grid-item">0</div>
-      <div className="decimal grid-item">.</div>
+      <div className="divide grid-item"><a href="#">/</a></div>
+      <div className="multiply grid-item"><a href="#">x</a></div>
+      <div className="seven grid-item"><a href="#">7</a></div>
+      <div className="eight grid-item"><a href="#">8</a></div>
+      <div className="nine grid-item"><a href="#">9</a></div>
+      <div className="subtract grid-item"><a href="#">-</a></div>
+      <div className="four grid-item"><a href="#">4</a></div>
+      <div className="five grid-item"><a href="#">5</a></div>
+      <div className="six grid-item"><a href="#">6</a></div>
+      <div className="add grid-item"><a href="#">+</a></div>
+      <div className="one grid-item"><a href="#">1</a></div>
+      <div className="two grid-item"><a href="#">2</a></div>
+      <div className="three grid-item"><a href="#">3</a></div>
+      <div className="equal grid-item"><a href="#">=</a></div>
+      <div className="zero grid-item"><a href="#">0</a></div>
+      <div className="decimal grid-item"><a href="#">.</a></div>
     </div>
   )
 }
