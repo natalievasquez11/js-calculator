c37ebf44ac0596d8d6a979f95a3c03e36dc00b1c
Natalie Vasquez <natalievasquez11@gmail.com>
2022-02-10 16:50:46 -0600

    started calculator functionality & fixed display with multiple zeros as inputs

--- src/components/calculatorContainer/CalculatorContainer.js
+++ src/components/calculatorContainer/CalculatorContainer.js
@@ -3,38 +3,75 @@ import './calculatorContainer.css';
 import ButtonsComponent from '../ButtonsComponent/ButtonsComponent';
 
 function CalculatorContainer() {
-  const [displayValue, setDisplayValue] = useState(0);
+  const [displayValue, setDisplayValue] = useState("0");
 
   const calculatorBtns = [
     { cName: "AC", value: "AC", id: "clear" },
     { cName: "divide", value: "/", id: "divide" },
     { cName: "multiply", value: "x", id: "multiply" },
-    { cName: "seven", value: 7, id: "seven" },
-    { cName: "eight", value: 8, id: "eight" },
-    { cName: "nine", value: 9, id: "nine" },
+    { cName: "seven", value: "7", id: "seven" },
+    { cName: "eight", value: "8", id: "eight" },
+    { cName: "nine", value: "9", id: "nine" },
     { cName: "subtract", value: "-", id: "subtract" },
-    { cName: "four", value: 4, id: "four" },
-    { cName: "five", value: 5, id: "five" },
-    { cName: "six", value: 6, id: "six" },
+    { cName: "four", value: "4", id: "four" },
+    { cName: "five", value: "5", id: "five" },
+    { cName: "six", value: "6", id: "six" },
     { cName: "add", value: "+", id: "add" },
-    { cName: "one", value: 1, id: "one" },
-    { cName: "two", value: 2, id: "two" },
-    { cName: "three", value: 3, id: "three" },
+    { cName: "one", value: "1", id: "one" },
+    { cName: "two", value: "2", id: "two" },
+    { cName: "three", value: "3", id: "three" },
     { cName: "equal", value: "=" , id: "equals"},
-    { cName: "zero", value: 0, id: "zero" },
+    { cName: "zero", value: "0", id: "zero" },
     { cName: "decimal", value: ".", id: "decimal" }
   ]
 
+  let operations = {
+    "+": function(a, b) { return a + b },
+    "-": function(a, b) { return a - b },
+    "*": function(a, b) { return a * b },
+    "/": function(a, b) { return a / b },
+  }
+
+  const solveEquation = (equationString) => {
+    let tempArrItem = equationString[0];
+    let eqArr = []
+    let total = 0;
+
+    for(let item = 1; item < equationString.length; item++) {
+      if(equationString[item] === "+" || equationString[item] === "-" || equationString[item] === "*" || equationString[item] === "/") {
+        eqArr.push(parseInt(tempArrItem), equationString[item]);
+        tempArrItem = "";
+      } else {
+        tempArrItem += equationString[item];
+      }
+    }
+    eqArr.push(parseInt(tempArrItem));
+    console.log(eqArr)
+
+    for(let x = 0; x < eqArr.length; x++) {
+
+    }
+
+    setDisplayValue('answer');
+  }
+
   const handleDisplayChange = (e) => {
     let value = e.target.value;
+
     if(value === "AC") {
-      setDisplayValue(0);
-    } else if(displayValue === 0) {
+      setDisplayValue("0");
+    } else if(value === "=") {
+      solveEquation(displayValue);
+    } else if(displayValue === "0" && value === 0) {
+      setDisplayValue(displayValue)
+    } else if(displayValue === "0") {
       setDisplayValue(value);
     } else {
       setDisplayValue(displayValue + value);
     }
   }
+
+
 
   const calcBtnInfo = calculatorBtns.map((btn, index) =>
     <ButtonsComponent 
